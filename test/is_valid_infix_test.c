8388a3d418985e7bf31a3273b6da905126d5154e
bluebirdsky <dnippa@pillartechology.com>
2016-09-19 21:53:21 -0400

    Test: modified test to make spaces invalid.

--- test/is_valid_infix_test.c
+++ test/is_valid_infix_test.c
@@ -71,9 +71,9 @@ START_TEST(WhenGiven_j_MULT_k_MULT_l_PLUS_m_PLUS_n_DIV_o_ReturnTrue)
 }
 END_TEST
 
-START_TEST(WhenGiven_p_POW_q_MULT_r_MULT_s_PLUS_t_PLUS_u_SPACE_ReturnTrue)
+START_TEST(WhenGiven_p_POW_q_MULT_r_MULT_s_PLUS_t_PLUS_u_ReturnTrue)
 {
-  bool return_value = is_valid_infix("p^q*r*s+t+u ");
+  bool return_value = is_valid_infix("p^q*r*s+t+u");
   ck_assert(return_value == true);
 }
 END_TEST
@@ -145,7 +145,7 @@ int main(void)
     tcase_add_test(tc1_1, WhenGiven_a_PLUS_b_PLUS_c_ReturnTrue);
     tcase_add_test(tc1_1, WhenGiven_d_PLUS_e_PLUS_f_PLUS_g_PLUS_h_PLUS_i_ReturnTrue);
     tcase_add_test(tc1_1, WhenGiven_j_MULT_k_MULT_l_PLUS_m_PLUS_n_DIV_o_ReturnTrue);
-    tcase_add_test(tc1_1, WhenGiven_p_POW_q_MULT_r_MULT_s_PLUS_t_PLUS_u_SPACE_ReturnTrue);
+    tcase_add_test(tc1_1, WhenGiven_p_POW_q_MULT_r_MULT_s_PLUS_t_PLUS_u_ReturnTrue);
     tcase_add_test(tc1_1, WhenGiven_OPEN_v_PLUS_w_CLOSED_MULT_x_MULT_y_PLUS_z_ReturnTrue);
     tcase_add_test(tc1_1, WhenGiven_Missing_Closed_Bracket_ReturnFalse);
     tcase_add_test(tc1_1, WhenGiven_Closed_Bracket_Closed_Bracket_ReturnFalse);
