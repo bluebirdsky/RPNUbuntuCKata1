d10d2a69db517636f98d424cc145474634bab8b6
bluebirdsky <dnippa@pillartechology.com>
2016-09-20 10:37:11 -0400

    Test: ab invalid.

--- test/is_valid_rpn_test.c
+++ test/is_valid_rpn_test.c
@@ -22,9 +22,9 @@ START_TEST(WhenGiven_b_a_PLUS_ReturnTrue)
 }
 END_TEST
 
-START_TEST(WhenGiven_b_a_ReturnFalse)
+START_TEST(WhenGiven_a_b_ReturnFalse)
 {
-  bool return_value = is_valid_rpn("ba");
+  bool return_value = is_valid_rpn("ab");
   ck_assert(return_value == false);
 }
 END_TEST
@@ -40,7 +40,7 @@ int main(void)
     tcase_add_test(tc1_1, WhenGivenEmptyStringReturnsFalse);
     tcase_add_test(tc1_1, WhenGiven_a_ReturnTrue);
     tcase_add_test(tc1_1, WhenGiven_b_a_PLUS_ReturnTrue);
-    tcase_add_test(tc1_1, WhenGiven_b_a_ReturnFalse);
+    tcase_add_test(tc1_1, WhenGiven_a_b_ReturnFalse);
 
     srunner_run_all(sr, CK_ENV);
     nf = srunner_ntests_failed(sr);
