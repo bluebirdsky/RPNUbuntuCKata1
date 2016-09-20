b852463c64e2b44a85d0b82ab0b327dc1b95d3d7
bluebirdsky <dnippa@pillartechology.com>
2016-09-20 15:52:29 -0400

    Refactor: removed tabs.

--- test/is_valid_infix_test.c
+++ test/is_valid_infix_test.c
@@ -143,36 +143,36 @@ END_TEST
 
 int main(void)
 {
-    Suite *s1 = suite_create("Core");
-    TCase *tc1_1 = tcase_create("is_valid_infix_tests");
-    SRunner *sr = srunner_create(s1);
-    int nf;
+  Suite *s1 = suite_create("Core");
+  TCase *tc1_1 = tcase_create("is_valid_infix_tests");
+  SRunner *sr = srunner_create(s1);
+  int nf;
 
-    suite_add_tcase(s1, tc1_1);
-    tcase_add_test(tc1_1, WhenGivenEmptyStringReturnsFalse);
-    tcase_add_test(tc1_1, WhenGivenNullReturnsFalse);
-    tcase_add_test(tc1_1, WhenGiven_1_ReturnFalse);
-    tcase_add_test(tc1_1, WhenGiven_2_ReturnFalse);
-    tcase_add_test(tc1_1, WhenGiven_3_ReturnFalse);
-    tcase_add_test(tc1_1, WhenGiven_a_ReturnTrue);
-    tcase_add_test(tc1_1, WhenGiven_a_PLUS_b_ReturnTrue);
-    tcase_add_test(tc1_1, WhenGiven_a_PLUS_b_PLUS_c_ReturnTrue);
-    tcase_add_test(tc1_1, WhenGiven_d_PLUS_e_PLUS_f_PLUS_g_PLUS_h_PLUS_i_ReturnTrue);
-    tcase_add_test(tc1_1, WhenGiven_j_MULT_k_MULT_l_PLUS_m_PLUS_n_DIV_o_ReturnTrue);
-    tcase_add_test(tc1_1, WhenGiven_p_POW_q_MULT_r_MULT_s_PLUS_t_PLUS_u_ReturnTrue);
-    tcase_add_test(tc1_1, WhenGiven_OPEN_v_PLUS_w_CLOSED_MULT_x_MULT_y_PLUS_z_ReturnTrue);
-    tcase_add_test(tc1_1, WhenGiven_Missing_Closed_Bracket_ReturnFalse);
-    tcase_add_test(tc1_1, WhenGiven_Closed_Bracket_Closed_Bracket_ReturnFalse);
-    tcase_add_test(tc1_1, WhenGiven_Closed_Bracket_Open_Bracket_ReturnFalse);
-    tcase_add_test(tc1_1, WhenGiven_Operand_Operand_Without_Operation_ReturnFalse);
-    tcase_add_test(tc1_1, WhenGiven_OPEN_a_PLUS_b_CLOSED_c_ReturnFalse);
-    tcase_add_test(tc1_1, WhenGiven_a_OPEN_b_PLUS_c_CLOSED_ReturnFalse);
-    tcase_add_test(tc1_1, WhenGiven_Operator_First_ReturnFalse);
-    tcase_add_test(tc1_1, WhenGiven_c_SUB_c_ReturnTrue);
+  suite_add_tcase(s1, tc1_1);
+  tcase_add_test(tc1_1, WhenGivenEmptyStringReturnsFalse);
+  tcase_add_test(tc1_1, WhenGivenNullReturnsFalse);
+  tcase_add_test(tc1_1, WhenGiven_1_ReturnFalse);
+  tcase_add_test(tc1_1, WhenGiven_2_ReturnFalse);
+  tcase_add_test(tc1_1, WhenGiven_3_ReturnFalse);
+  tcase_add_test(tc1_1, WhenGiven_a_ReturnTrue);
+  tcase_add_test(tc1_1, WhenGiven_a_PLUS_b_ReturnTrue);
+  tcase_add_test(tc1_1, WhenGiven_a_PLUS_b_PLUS_c_ReturnTrue);
+  tcase_add_test(tc1_1, WhenGiven_d_PLUS_e_PLUS_f_PLUS_g_PLUS_h_PLUS_i_ReturnTrue);
+  tcase_add_test(tc1_1, WhenGiven_j_MULT_k_MULT_l_PLUS_m_PLUS_n_DIV_o_ReturnTrue);
+  tcase_add_test(tc1_1, WhenGiven_p_POW_q_MULT_r_MULT_s_PLUS_t_PLUS_u_ReturnTrue);
+  tcase_add_test(tc1_1, WhenGiven_OPEN_v_PLUS_w_CLOSED_MULT_x_MULT_y_PLUS_z_ReturnTrue);
+  tcase_add_test(tc1_1, WhenGiven_Missing_Closed_Bracket_ReturnFalse);
+  tcase_add_test(tc1_1, WhenGiven_Closed_Bracket_Closed_Bracket_ReturnFalse);
+  tcase_add_test(tc1_1, WhenGiven_Closed_Bracket_Open_Bracket_ReturnFalse);
+  tcase_add_test(tc1_1, WhenGiven_Operand_Operand_Without_Operation_ReturnFalse);
+  tcase_add_test(tc1_1, WhenGiven_OPEN_a_PLUS_b_CLOSED_c_ReturnFalse);
+  tcase_add_test(tc1_1, WhenGiven_a_OPEN_b_PLUS_c_CLOSED_ReturnFalse);
+  tcase_add_test(tc1_1, WhenGiven_Operator_First_ReturnFalse);
+  tcase_add_test(tc1_1, WhenGiven_c_SUB_c_ReturnTrue);
 
-    srunner_run_all(sr, CK_ENV);
-    nf = srunner_ntests_failed(sr);
-    srunner_free(sr);
+  srunner_run_all(sr, CK_ENV);
+  nf = srunner_ntests_failed(sr);
+  srunner_free(sr);
 
-    return nf == 0 ? 0 : 1;
+  return nf == 0 ? 0 : 1;
 }
