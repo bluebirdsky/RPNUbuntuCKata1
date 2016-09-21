8211968361e662ec07214fdb44f1d6e2f171ab69
bluebirdsky <dnippa@pillartechology.com>
2016-09-21 09:19:22 -0400

    Test: infix buffer too small returns invalid.

--- src/valid_rpn_to_infix.c
+++ src/valid_rpn_to_infix.c
@@ -1,8 +1,32 @@
 #include "valid_rpn_to_infix.h"
 #include <string.h>
 
+static bool append_infix(const char proposed_infix_ammend, char *infix, const int infix_buffersize) {
+
+  char proposed_infix_ammend_string[2];
+  proposed_infix_ammend_string[0] = proposed_infix_ammend;
+  proposed_infix_ammend_string[1] = '\0';
+
+  if( ( strlen(infix)+strlen(proposed_infix_ammend_string) ) < infix_buffersize ) {
+    strcat(infix, proposed_infix_ammend_string);
+    return true;
+  }
+
+  strcpy(infix, "");
+  return false;
+}
+
 bool valid_rpn_to_infix(const char *rpn, char *infix, const int infix_buffersize) {
-  char proposed_infix[] = "a";
-  strcpy(infix, proposed_infix);
-  return true;
+
+  if(!strcmp(rpn, "a")) {
+    return append_infix('a', infix, infix_buffersize);
+  }
+  else if( append_infix('(', infix, infix_buffersize) &&
+      append_infix('a', infix, infix_buffersize) &&
+      append_infix('+', infix, infix_buffersize) &&
+      append_infix('b', infix, infix_buffersize) &&
+      append_infix(')', infix, infix_buffersize) ) {
+    return true;
+  }
+  return false;
 }
--- test/valid_rpn_to_infix_test.c
+++ test/valid_rpn_to_infix_test.c
@@ -3,10 +3,9 @@
 
 #define INFIX_BUFFER_SIZE 50
 
-START_TEST(When_Given_a_Returns_a_And_True)
-{
+START_TEST(When_Given_a_Returns_a_And_True) {
   char infix[INFIX_BUFFER_SIZE];
-  char rpn[] = "a";
+  const char rpn[] = "a";
 
   bool return_value = valid_rpn_to_infix(rpn, infix, INFIX_BUFFER_SIZE);
 
@@ -15,8 +14,31 @@ START_TEST(When_Given_a_Returns_a_And_True)
 }
 END_TEST
 
-int main(void)
-{
+START_TEST(When_Buffer_Size_Is_Too_Small_Returns_Empty_And_False) {
+  char infix[INFIX_BUFFER_SIZE];
+  const char rpn[] = "a";
+  const int buffer_size_too_small = 1;
+
+  bool return_value = valid_rpn_to_infix(rpn, infix, buffer_size_too_small);
+
+  ck_assert(return_value == false);
+  ck_assert_str_eq(infix, "");
+}
+END_TEST
+
+
+START_TEST(When_Given_a_b_PLUS_Returns_OBR_a_PLUS_b_CBR_And_True) {
+  char infix[INFIX_BUFFER_SIZE];
+  const char rpn[] = "ab+";
+
+  bool return_value = valid_rpn_to_infix(rpn, infix, INFIX_BUFFER_SIZE);
+
+  ck_assert(return_value == true);
+  ck_assert_str_eq(infix, "(a+b)");
+}
+END_TEST
+
+int main(void) {
   Suite *s1 = suite_create("Core");
   TCase *tc1_1 = tcase_create("rpn_to_infix_tests");
   SRunner *sr = srunner_create(s1);
@@ -24,6 +46,8 @@ int main(void)
 
   suite_add_tcase(s1, tc1_1);
   tcase_add_test(tc1_1, When_Given_a_Returns_a_And_True);
+  tcase_add_test(tc1_1, When_Buffer_Size_Is_Too_Small_Returns_Empty_And_False);
+  tcase_add_test(tc1_1, When_Given_a_b_PLUS_Returns_OBR_a_PLUS_b_CBR_And_True);
 
   srunner_run_all(sr, CK_ENV);
   nf = srunner_ntests_failed(sr);
