a3866440f10509bb878f646cf20f50c9f417fe8b
bluebirdsky <dnippa@pillartechology.com>
2016-09-23 11:13:48 -0400

    Test: Adding test coverage lm+no-p+qrst^*+^* = (l+m)*(((n-o)+p)^(q+(r*(s^t))))

--- test/valid_rpn_to_infix_test.c
+++ test/valid_rpn_to_infix_test.c
@@ -70,6 +70,17 @@ START_TEST(When_Given_i_j_h_k_POW_DIV_SUB_Returns_i_SUB_OBR_j_DIV_OBR_h_POW_k_2C
 }
 END_TEST
 
+START_TEST(When_Given_l_m_PLUS_n_o_SUB_p_PLUS_q_r_s_t_POW_MULT_PLUS_POW_MULT_Returns_OBR_l_PLUS_m_CBR_MULT_3OBR_n_SUB_o_CBR_PLUS_p_CBR_POW_OBR_r_MULT_OBR_r_OBR_s_POW_t_4CBR_And_Success) {
+  char infix[INFIX_BUFFER_SIZE];
+  const char rpn[] = "lm+no-p+qrst^*+^*";
+
+  bool return_value = valid_rpn_to_infix(rpn, infix, INFIX_BUFFER_SIZE);
+
+  ck_assert(return_value == false);
+  ck_assert_str_eq(infix, "(l+m)*(((n-o)+p)^(q+(r*(s^t))))");
+}
+END_TEST
+
 int main(void) {
   Suite *s1 = suite_create("Core");
   TCase *tc1_1 = tcase_create("rpn_to_infix_tests");
@@ -83,6 +94,7 @@ int main(void) {
   tcase_add_test(tc1_1, When_Given_c_d_e_PLUS_PLUSReturns_c_PLUS_OBR_d_PLUS_e_CBR_And_Success);
   tcase_add_test(tc1_1, When_Given_f_g_SUB_h_MULT_Returns_OBR_f_SUB_g_CBR_MULT_h_And_Success);
   tcase_add_test(tc1_1, When_Given_i_j_h_k_POW_DIV_SUB_Returns_i_SUB_OBR_j_DIV_OBR_h_POW_k_2CBR_And_Success);
+  tcase_add_test(tc1_1, When_Given_l_m_PLUS_n_o_SUB_p_PLUS_q_r_s_t_POW_MULT_PLUS_POW_MULT_Returns_OBR_l_PLUS_m_CBR_MULT_3OBR_n_SUB_o_CBR_PLUS_p_CBR_POW_OBR_r_MULT_OBR_r_OBR_s_POW_t_4CBR_And_Success);
 
   srunner_run_all(sr, CK_ENV);
   nf = srunner_ntests_failed(sr);
