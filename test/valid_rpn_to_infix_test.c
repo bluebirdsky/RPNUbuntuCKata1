785a7d3bcf507eee180f98514357e6b6e9dc0e8e
bluebirdsky <dnippa@pillartechology.com>
2016-09-23 13:49:39 -0400

    Test: Added test coverage: lmn^/o*p- = ((l/(m^n))*o)-p success.

--- test/valid_rpn_to_infix_test.c
+++ test/valid_rpn_to_infix_test.c
@@ -92,6 +92,17 @@ START_TEST(When_Given_v_w_DIV_x_POW_y_z_SUB_MULT_Returns_2OBR_v_DEV_w_CBR_POW_x_
 }
 END_TEST
 
+START_TEST(When_Given_l_m_n_POW_DIV_o_MULT_p_SUB_Returns_2OBR_l_DIV_OBR_m_POW_n_2CBR_MULT_o_CBR_SUB_p_And_Success) {
+  char infix[INFIX_BUFFER_SIZE];
+  const char rpn[] = "lmn^/o*p-";
+
+  bool return_value = valid_rpn_to_infix(rpn, infix, INFIX_BUFFER_SIZE);
+
+  ck_assert(return_value == false);
+  ck_assert_str_eq(infix, "((l/(m^n))*o)-p");
+}
+END_TEST
+
 int main(void) {
   Suite *s1 = suite_create("Core");
   TCase *tc1_1 = tcase_create("rpn_to_infix_tests");
@@ -107,6 +118,7 @@ int main(void) {
   tcase_add_test(tc1_1, When_Given_i_j_h_k_POW_DIV_SUB_Returns_i_SUB_OBR_j_DIV_OBR_h_POW_k_2CBR_And_Success);
   tcase_add_test(tc1_1, When_Given_l_m_PLUS_n_o_SUB_p_PLUS_q_r_s_t_POW_MULT_PLUS_POW_MULT_Returns_OBR_l_PLUS_m_CBR_MULT_3OBR_n_SUB_o_CBR_PLUS_p_CBR_POW_OBR_r_MULT_OBR_r_OBR_s_POW_t_4CBR_And_Success);
   tcase_add_test(tc1_1, When_Given_v_w_DIV_x_POW_y_z_SUB_MULT_Returns_2OBR_v_DEV_w_CBR_POW_x_CBR_MULT_OBR_y_SUB_z_CBR_And_Success);
+  tcase_add_test(tc1_1, When_Given_l_m_n_POW_DIV_o_MULT_p_SUB_Returns_2OBR_l_DIV_OBR_m_POW_n_2CBR_MULT_o_CBR_SUB_p_And_Success);
 
   srunner_run_all(sr, CK_ENV);
   nf = srunner_ntests_failed(sr);
