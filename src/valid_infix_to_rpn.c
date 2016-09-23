8b091cd93ccec565531ae4bb3fd819883f549c82
bluebirdsky <dnippa@pillartechology.com>
2016-09-23 15:55:53 -0400

    Test: a+b=ab+ valid. Also, corrected other test.

--- src/valid_infix_to_rpn.c
+++ src/valid_infix_to_rpn.c
@@ -4,5 +4,15 @@
 #include <string.h>
 
 bool valid_infix_to_rpn(const char *infix, char *rpn, const int rpn_buffersize) {
-  return append_string(infix[0], rpn, rpn_buffersize);
+  int was_buffer_exceeded = true;
+
+  if(!strcmp(infix, "a")) {
+    return append_string(infix[0], rpn, rpn_buffersize);
+  }
+  else {
+    was_buffer_exceeded = append_string(infix[0], rpn, rpn_buffersize);
+    was_buffer_exceeded = append_string(infix[2], rpn, rpn_buffersize);
+    was_buffer_exceeded = append_string(infix[1], rpn, rpn_buffersize);
+    return was_buffer_exceeded;
+  }
 }
--- test/valid_infix_to_rpn_test.c
+++ test/valid_infix_to_rpn_test.c
@@ -10,7 +10,7 @@ START_TEST(When_Given_a_Returns_a_And_Success) {
   bool return_value = valid_infix_to_rpn(infix, rpn, RPN_BUFFER_SIZE);
 
   ck_assert(return_value == false);
-  ck_assert_str_eq(infix, "a");
+  ck_assert_str_eq(rpn, "a");
 }
 END_TEST
 
@@ -26,6 +26,17 @@ START_TEST(When_Buffer_Size_Is_Too_Small_Returns_Empty_And_Failure) {
 }
 END_TEST
 
+START_TEST(When_Given_a_PLUS_b_Returns_a_b_PLUS_And_Success) {
+  char rpn[RPN_BUFFER_SIZE];
+  const char infix[] = "a+b";
+
+  bool return_value = valid_infix_to_rpn(infix, rpn, RPN_BUFFER_SIZE);
+
+  ck_assert(return_value == false);
+  ck_assert_str_eq(rpn, "ab+");
+}
+END_TEST
+
 int main(void) {
   Suite *s1 = suite_create("Core");
   TCase *tc1_1 = tcase_create("valid_infix_to_rpn_tests");
@@ -35,6 +46,7 @@ int main(void) {
   suite_add_tcase(s1, tc1_1);
   tcase_add_test(tc1_1, When_Given_a_Returns_a_And_Success);
   tcase_add_test(tc1_1, When_Buffer_Size_Is_Too_Small_Returns_Empty_And_Failure);
+  tcase_add_test(tc1_1, When_Given_a_PLUS_b_Returns_a_b_PLUS_And_Success);
 
   srunner_run_all(sr, CK_ENV);
   nf = srunner_ntests_failed(sr);
