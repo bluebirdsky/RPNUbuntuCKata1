32fd18435d0796883d6952aa851e0ba51fb45b80
bluebirdsky <dnippa@pillartechology.com>
2016-09-23 15:28:26 -0400

    Test: RPN buffer size too small. Failed

--- src/valid_infix_to_rpn.c
+++ src/valid_infix_to_rpn.c
@@ -3,6 +3,14 @@
 #include <string.h>
 
 bool valid_infix_to_rpn(const char *infix, char *rpn, const int rpn_buffersize) {
-  strcpy(rpn, infix);
-  return false;
+  char proposed_rpn_ammend_string[2];
+  proposed_rpn_ammend_string[0] = infix[0];
+  proposed_rpn_ammend_string[1] = '\0';
+
+  if( ( strlen(rpn)+strlen(proposed_rpn_ammend_string) ) < rpn_buffersize ) {
+    strcat(rpn, proposed_rpn_ammend_string);
+    return false;
+  }
+  strcpy(rpn, "");
+  return true;
 }
