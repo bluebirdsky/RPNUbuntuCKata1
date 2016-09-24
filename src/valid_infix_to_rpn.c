a1f8c14b961572dee6bc01c028ac1fdacd61527f
bluebirdsky <dnippa@pillartechology.com>
2016-09-24 07:10:11 -0400

    Test: g = g success.

--- src/valid_infix_to_rpn.c
+++ src/valid_infix_to_rpn.c
@@ -9,21 +9,22 @@ bool valid_infix_to_rpn(const char *infix, char *rpn, const int rpn_buffersize) 
   int infix_length = strlen(infix);
   char operator = ' ';
 
-  if(!strcmp(infix, "a")) {
-    return append_string(infix[0], rpn, rpn_buffersize);
-  }
-  else {
-    for( i=0; i < infix_length; ++i ) {
-      if( is_open_bracket(infix[i]) & is_closed_bracket(infix[i]) ) {
-        continue;
-      }
-      else if( is_operand(infix[i]) ) {
-        was_buffer_exceeded = append_string(infix[i], rpn, rpn_buffersize);
-      }
-      else if( is_operator(infix[i]) ) {
-        operator = infix[i];
-      }
+  for( i=0; i < infix_length; ++i ) {
+    if( is_open_bracket(infix[i]) & is_closed_bracket(infix[i]) ) {
+      continue;
     }
-    return append_string(operator, rpn, rpn_buffersize);
+    else if( is_operand(infix[i]) ) {
+      was_buffer_exceeded = append_string(infix[i], rpn, rpn_buffersize);
+    }
+    else if( is_operator(infix[i]) ) {
+      operator = infix[i];
+    }
   }
+
+  if( operator != ' ' ) {
+    was_buffer_exceeded = append_string(operator, rpn, rpn_buffersize);
+  }
+
+  return was_buffer_exceeded;
+
 }
