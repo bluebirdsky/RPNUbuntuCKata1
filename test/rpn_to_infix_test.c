b08c6297325b1b6ae503f8739bc2531b48b85639
bluebirdsky <dnippa@pillartechology.com>
2016-09-27 14:20:06 -0400

    Test: added test coverage for buffer too small.

--- test/rpn_to_infix_test.c
+++ test/rpn_to_infix_test.c
@@ -15,6 +15,19 @@ START_TEST(When_Given_a_Returns_a_And_Success) {
 }
 END_TEST
 
+START_TEST(When_Buffer_Size_Is_Too_Small_Returns_Empty_And_Failure) {
+  char infix[INFIX_BUFFER_SIZE];
+  const char rpn[] = "a";
+  const int buffer_size_too_small = 1;
+  strcpy(infix,"");
+
+  bool return_value = valid_rpn_to_infix(rpn, infix, buffer_size_too_small);
+
+  ck_assert(return_value == true);
+  ck_assert_str_eq(infix, "");
+}
+END_TEST
+
 int main(void) {
   Suite *s1 = suite_create("Core");
   TCase *tc1_1 = tcase_create("rpn_to_infix_tests");
@@ -23,6 +36,8 @@ int main(void) {
 
   suite_add_tcase(s1, tc1_1);
   tcase_add_test(tc1_1, When_Given_a_Returns_a_And_Success);
+  tcase_add_test(tc1_1, When_Buffer_Size_Is_Too_Small_Returns_Empty_And_Failure);
+
   srunner_run_all(sr, CK_ENV);
   nf = srunner_ntests_failed(sr);
   srunner_free(sr);
